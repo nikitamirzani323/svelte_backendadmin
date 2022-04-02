c69882f15f627f29f6fac0a4e0dac3986ba9cd48
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-04-02 13:01:45 +0700

    Golang + Svelte + Vite + Tailwind

---
+++ routers/router.go
@@ -0,0 +1,25 @@
+package routers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/nikitamirzani323/svelte_backendadmin/controller"
+)
+
+func Init() *fiber.App {
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(compress.New())
+	app.Static("/", "frontend/public", fiber.Static{
+		Compress:  true,
+		ByteRange: true,
+		Browse:    true,
+	})
+
+	app.Get("api/healthz", controller.HealthCheck)
+	app.Post("api/init", controller.InitToken)
+	return app
+}
