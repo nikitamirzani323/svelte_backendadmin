6730b3d2ec743d27870a4e6cf5ef6694dfabd828
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-06-29 00:49:18 +0700

    update docker compose + path

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,8 @@ import App from './App.svelte'
 const app = new App({
   target: document.getElementById('app'),
   props: {
-		// path_api: "/",
-		path_api: "http://localhost:5051/",
+		path_api: "/",
+		// path_api: "http://localhost:5051/",
 		font_size: "text-[12px] lg:text-[13px]"
 	}
 })
